6f631c74047f3b18c8b196a72683fe8bb0be6efa
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-02-13 20:18:55 -0300

    Criação da interface DAO de Cartão

---
+++ src/main/java/DAOs/CardDAO.java
@@ -0,0 +1,21 @@
+package DAOs;
+
+import Entities.Card;
+import Exceptions.CardNotFoundException;
+
+import java.util.List;
+
+public interface CardDAO {
+	String insert(Card card);
+	
+	void update(Card card)
+		throws CardNotFoundException;
+	
+	void delete(String code)
+		throws CardNotFoundException;
+	
+	Card getCard(String code)
+		throws CardNotFoundException;
+	
+	List<Card> getAllCards();
+}
