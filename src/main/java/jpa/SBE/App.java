0e2b75cb1c59cb1bf4657f57f745bc17ef068d72
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-02-13 19:50:40 -0300

    Início do Projeto

---
+++ src/main/java/jpa/SBE/App.java
@@ -0,0 +1,13 @@
+package jpa.SBE;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
