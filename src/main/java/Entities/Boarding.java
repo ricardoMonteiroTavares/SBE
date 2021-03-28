b47988ffa9b3bc15e5fb3bab8dbeb4fdd161d2a5
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-03-28 19:48:28 -0300

    Criação do objeto de Boarding (Embarque)

---
+++ src/main/java/Entities/Boarding.java
@@ -0,0 +1,93 @@
+package Entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Table(name="boarding")
+public class Boarding {
+	
+	// Variáveis do Objeto
+	private Long id;
+	private Long id_executeTravel;
+	private Long id_card;
+	private Date date;
+	private String time;
+	
+	private int version;
+	
+	// Construtores
+	public Boarding(){}
+	
+	public Boarding(Long id_executeTravel, Long id_card, Date date, String time) {
+		this.setId_executeTravel(id_executeTravel);
+		this.setId_card(id_card);
+		this.setDate(date);
+		this.setTime(time);
+	}
+
+	// Getters
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="ID")
+	public Long getId() {
+		return id;
+	}
+	
+	public Long getId_executeTravel() {
+		return id_executeTravel;
+	}
+	
+	public Long getId_card() {
+		return id_card;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+	
+	@Version
+	public int getVersion() {
+		return version;
+	}
+
+	// Setters
+	@SuppressWarnings("unused")
+	private void setId(Long id) {
+		this.id = id;
+	}	
+
+	public void setId_executeTravel(Long id_executeTravel) {
+		this.id_executeTravel = id_executeTravel;
+	}
+
+	public void setId_card(Long id_card) {
+		this.id_card = id_card;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+	
+	
+}
