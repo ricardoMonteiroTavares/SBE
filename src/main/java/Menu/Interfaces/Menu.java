e72d472c942547f0b4a85b05fe9f71843db5b9ca
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-04-02 17:36:08 -0300

    Adição de mais métodos padrões em Menu

--- src/main/java/Menu/Interfaces/Menu.java
+++ src/main/java/Menu/Interfaces/Menu.java
@@ -1,8 +1,14 @@
 package Menu.Interfaces;
 
-public abstract class Menu {
+public abstract class Menu<T> {
 	
 	abstract public void menu();
 	
 	abstract protected void consoleOptions();
+	
+	abstract protected void insert(T obj);
+	
+	abstract protected void update(T obj);
+	
+	abstract protected void delete(T obj);
 }
