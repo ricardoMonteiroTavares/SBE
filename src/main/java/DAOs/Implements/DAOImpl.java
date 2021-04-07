972da33fa21fde04d6ecc3adcaeb6a76db4173d9
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-04-07 20:23:28 -0300

    Criação da implementação do DAO genérico

---
+++ src/main/java/DAOs/Implements/DAOImpl.java
@@ -0,0 +1,134 @@
+package DAOs.Implements;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.LockModeType;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+
+import Annotations.Execute;
+import DAOs.Interfaces.DAO;
+import Exceptions.ObjectNotFoundException;
+
+
+public class DAOImpl<T, PK> implements DAO<T, PK> {
+	private Class<T> type;
+
+	protected EntityManager em; 
+
+	public DAOImpl(Class<T> type) {
+		this.type = type;
+	}
+
+	@Execute
+	public T insert(T o) {
+		try {
+			em.persist(o);
+		} catch (RuntimeException e) {
+			throw e;
+		}
+
+		return o;
+	}
+
+	@Execute
+	public void update(T o) {
+		try {
+			em.merge(o);
+		} catch (RuntimeException e) {
+			throw e;
+		}
+	}
+
+	@Execute
+	public void delete(T o) {
+		try {
+			em.remove(o);
+		} catch (RuntimeException e) {
+			throw e;
+		}
+	}
+
+	@Execute
+	public T get(PK id) throws ObjectNotFoundException {
+		T t = null;
+		try {
+			t = em.find(type, id);
+
+			if (t == null) {
+				throw new ObjectNotFoundException("Elemento não encontrado");
+			}
+		} catch (RuntimeException e) {
+			throw e;
+		}
+		return t;
+	}
+
+	@Execute
+	public T getByLockMode(PK id) throws ObjectNotFoundException {
+		T t = null;
+		try {
+			t = em.find(type, id, LockModeType.PESSIMISTIC_WRITE);
+
+			if (t == null) {
+				throw new ObjectNotFoundException("Elemento não encontrado");
+			}
+		} catch (RuntimeException e) {
+			throw e;
+		}
+
+		return t;
+	}
+
+	@SuppressWarnings("unchecked")
+	public final T query(Method method, Object[] args) throws ObjectNotFoundException {
+		T t = null;
+		try {
+			String search = convertMethodToString(method);
+
+//			"select p from Produto p 
+//			"left outer join fetch p.lances "
+//			+ "where p.id = ?1"
+
+			Query namedQuery = em.createNamedQuery(search);
+
+			if (args != null) {
+				for (int i = 0; i < args.length; i++) {
+					Object arg = args[i];
+					namedQuery.setParameter(i + 1, arg); 
+				}
+			}
+			t = (T) namedQuery.getSingleResult();
+
+			return t;
+		} catch (NoResultException e) {
+			throw new ObjectNotFoundException("Elemento não encontrado");
+		} catch (RuntimeException e) {
+			throw e;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public final List<T> queryList(Method method, Object[] args) {
+		try {
+			String search = convertMethodToString(method);
+			Query namedQuery = em.createNamedQuery(search);
+
+			if (args != null) {
+				for (int i = 0; i < args.length; i++) {
+					Object arg = args[i];
+					namedQuery.setParameter(i + 1, arg); 
+				}
+			}
+			return (List<T>) namedQuery.getResultList();
+		} catch (RuntimeException e) {
+			throw e;
+		}
+	}
+
+	private String convertMethodToString(Method metodo) {
+		return type.getSimpleName() + "." + metodo.getName();
+	}
+}
