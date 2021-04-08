ac461b34acc979ad5e65a4a096704cd2e26c14c3
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-04-08 11:10:19 -0300

    Ajustes em BoardingService

--- src/main/java/Services/Interfaces/BoardingService.java
+++ src/main/java/Services/Interfaces/BoardingService.java
@@ -7,13 +7,16 @@ import Entities.Boarding;
 import Exceptions.ObjectNotFoundException;
 
 public interface BoardingService {
-	Boarding insert(Boarding boarding);
-
-	void delete(Boarding id)
+	Boarding insert(Boarding boarding) 
 		throws ObjectNotFoundException;
 
-	Boarding get(Long code)
-			throws ObjectNotFoundException;
+	void delete(Boarding boarding)
+		throws ObjectNotFoundException;
+
+	Boarding get(Long id)
+		throws ObjectNotFoundException;
 
 	List<Boarding> getAllBoardingsByDate(Long cardCode, Date date);
+	
+	List<Boarding> getAllBoardingsByPeriod(Long cardCode, Date start, Date finish);
 }
