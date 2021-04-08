edea9d08ef42f2ec3f7981f3c5443754326568c6
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-04-07 21:44:05 -0300

    Ajustes no DAOFactory

--- src/main/java/Factories/DAOFactory.java
+++ src/main/java/Factories/DAOFactory.java
@@ -1,48 +1,26 @@
 package Factories;
 
-import java.util.MissingResourceException;
-import java.util.ResourceBundle;
+import java.util.Set;
+
+import org.reflections.Reflections;
+
+import Proxy.DAOIntercept;
+import net.sf.cglib.proxy.Enhancer;
 
 public class DAOFactory {
-	private static ResourceBundle prop;
-
-	static
-	{	try
-		{	prop = ResourceBundle.getBundle("dao");
-		}
-		catch(MissingResourceException e)
-		{	System.out.println("Aquivo dao.properties não encontrado.");
-			throw new RuntimeException(e);
-		}
-	}
 
 	@SuppressWarnings("unchecked")
 	public static <T> T getDAO(Class<T> type)
 	{			
-		T dao = null;
-		String className = null; 
-	
-		try
-		{	className = prop.getString(type.getSimpleName());
-			dao = (T) Class.forName(className).newInstance();
-		} 
-		catch (InstantiationException e)
-		{	System.out.println("Não foi possível criar um objeto do tipo " + className);
-			throw new RuntimeException(e);
-		} 
-		catch (IllegalAccessException e)
-		{	System.out.println("Não foi possível criar um objeto do tipo " + className);
-			throw new RuntimeException(e);
-		} 
-		catch (ClassNotFoundException e)
-		{	System.out.println("Classe " + className + " não encontrada");
-			throw new RuntimeException(e);
-		}
-		catch(MissingResourceException e)
-		{	System.out.println("Chave " + type + " não encontrada em dao.properties");
-			throw new RuntimeException(e);
-		}
-		
-		return dao;
+		Reflections reflections = new Reflections("dao.impl");
+
+		Set<Class<? extends T>> classes = reflections.getSubTypesOf(type);
+
+		if (classes.size() > 1)
+			throw new RuntimeException("Somente uma classe pode implementar " + type.getName());
+
+		Class<?> classe = classes.iterator().next();
+
+		return (T) Enhancer.create(classe, new DAOIntercept());
 	}
 }
