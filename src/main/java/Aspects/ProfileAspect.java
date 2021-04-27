4a0085cbe6c4d7aab1fe3b1b8ed6aeaa9f8340dd
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-04-27 20:06:57 -0300

    Criação do aspecto para detectar se o perfil logado deve pertence a lista de perfis permitidos para executar o método

---
+++ src/main/java/Aspects/ProfileAspect.java
@@ -0,0 +1,50 @@
+package Aspects;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+
+import Annotations.Profile;
+import Exceptions.ProfileException;
+import Singletons.ProfileSingleton;
+
+@Aspect
+public class ProfileAspect {
+		
+	@Pointcut("call(* Services.Interfaces..*.*(..))")
+	public void detectProfile() {}
+	
+	@Around("detectProfile()")
+	public Object detectProfileEx(ProceedingJoinPoint joinPoint) throws Throwable 
+	{
+		MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+		String[] loggedProfiles = ProfileSingleton.getSingleton().getProfiles();
+		
+		if(signature.getMethod().isAnnotationPresent(Profile.class)) 
+		{
+			String[] profiles = signature.getMethod().getAnnotation(Profile.class).profiles();
+			
+			for(String loggedProfile : loggedProfiles) 
+			{
+				for(String profile : profiles)
+				{
+					if(profile == loggedProfile) 
+					{
+						return joinPoint.proceed();
+					}
+				}
+			}
+			
+			throw new ProfileException("Perfil inválido");
+		}
+		else 
+		{
+			return joinPoint.proceed();
+		}
+		
+	}
+		
+	
+}
